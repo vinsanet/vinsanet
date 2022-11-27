6fde9e3f37ddc239ed1f6c10d4380361945834eb
hnymA <hnyma637111@gmail.com>
2022-11-27 21:41:15 +0900

    remove comments

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,7 @@
-/**
- * main.ts
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
-// Components
 import App from "./App.vue";
 
-// Composables
 import { createApp } from "vue";
 
-// Plugins
 import { registerPlugins } from "@/plugins";
 import router from "@/router";
 
--- src/plugins/webfontloader.ts
+++ src/plugins/webfontloader.ts
@@ -1,12 +1,7 @@
-/**
- * plugins/webfontloader.ts
- *
- * webfontloader documentation: https://github.com/typekit/webfontloader
- */
-
 export async function loadFonts() {
   const webFontLoader = await import(
-    /* webpackChunkName: "webfontloader" */ "webfontloader"
+    /* webpackChunkName: "webfontloader" */
+    "webfontloader"
   );
 
   webFontLoader.load({
