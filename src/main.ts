ea34dff22c3ce055a6e46d4785db506b28806e77
hnymA <hnyma637111@gmail.com>
2022-11-28 23:09:46 +0900

    add initial settings

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
-import App from "./App.vue";
-
-import { createApp } from "vue";
-
+import App from "@/App.vue";
 import { registerPlugins } from "@/plugins";
 import router from "@/router";
+import { createApp } from "vue";
 
 const app = createApp(App);
 
