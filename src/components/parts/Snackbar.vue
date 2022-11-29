41ee442fae5f919571fe8ac043bcc80e57b886ff
hnymA <hnyma637111@gmail.com>
2022-11-29 16:57:47 +0900

    add snackbar

---
+++ src/components/parts/Snackbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-snackbar v-model="shouldShow" text :color="color">
+    {{ body }}
+    <template #actions>
+      <v-btn icon @click="shouldShow = false">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+
+<script setup lang="ts">
+  import { useSnackbarStore } from "@/store/snackbar";
+  import { storeToRefs } from "pinia";
+
+  const snackbarStore = useSnackbarStore();
+  const { body, color, shouldShow } = storeToRefs(snackbarStore);
+</script>
