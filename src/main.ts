bf5bbe53e56a735664c4caebe261685c084d8ba7
hnymA <hnyma637111@gmail.com>
2022-11-29 16:51:24 +0900

    add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import App from "@/App.vue";
 import { registerPlugins } from "@/plugins";
 import router from "@/router";
+import { createPinia } from "pinia";
 import { createApp } from "vue";
 
+const pinia = createPinia();
 const app = createApp(App);
 
 registerPlugins(app);
 
 app.use(router);
+app.use(pinia);
 app.mount("#app");
