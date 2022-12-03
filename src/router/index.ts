cd9434970ae53af70209ec79fd21325fcd47e4ee
hnymA <hnyma637111@gmail.com>
2022-12-03 14:55:52 +0900

    add routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
 import Account from "@/components/pages/Account.vue";
+import CharactersIdEdit from "@/components/pages/CharactersIdEdit.vue";
+import CharactersIdView from "@/components/pages/CharactersIdView.vue";
 import Create from "@/components/pages/Create.vue";
 import Index from "@/components/pages/Index.vue";
-import Lists from "@/components/pages/Lists.vue";
 import Login from "@/components/pages/Login.vue";
+import Mypage from "@/components/pages/Mypage.vue";
 import Register from "@/components/pages/Register.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
@@ -10,8 +12,18 @@ const routes = [
   { path: "/", name: "index", component: Index },
   { path: "/account", name: "account", component: Account },
   { path: "/create", name: "create", component: Create },
-  { path: "/lists", name: "lists", component: Lists },
+  {
+    path: "/characters/:id/edit",
+    name: "characters-id-edit",
+    component: CharactersIdEdit,
+  },
+  {
+    path: "/characters/:id/view",
+    name: "characters-id-view",
+    component: CharactersIdView,
+  },
   { path: "/login", name: "login", component: Login },
+  { path: "/mypage", name: "mypage", component: Mypage },
   { path: "/register", name: "register", component: Register },
 ];
 
