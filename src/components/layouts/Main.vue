8613ee7a04c31d1143cf804caf859aa355193aa0
hnymA <hnyma637111@gmail.com>
2022-12-18 11:13:43 +0900

    add layouts

---
+++ src/components/layouts/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <Header :on-click-nav-icon="onClickNavIcon" />
+  <Drawer :drawer="drawer" />
+  <v-main>
+    <router-view />
+    <Snackbar />
+  </v-main>
+</template>
+
+<script setup lang="ts">
+  import Drawer from "@/components/parts/Drawer.vue";
+  import Header from "@/components/parts/Header.vue";
+  import Snackbar from "@/components/parts/Snackbar.vue";
+  import { ref } from "vue";
+  import { RouterView } from "vue-router";
+
+  const drawer = ref(false);
+
+  const onClickNavIcon = () => {
+    drawer.value = !drawer.value;
+  };
+</script>
---
+++ src/components/layouts/Simple.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-main>
+    <router-view />
+    <Snackbar />
+  </v-main>
+</template>
+
+<script setup lang="ts">
+  import Snackbar from "@/components/parts/Snackbar.vue";
+  import { RouterView } from "vue-router";
+</script>
