9f88ade0f8b35298891b6f66d78977e00c75f234
hnymA <hnyma637111@gmail.com>
2022-12-19 18:10:19 +0900

    add twitter button

---
+++ src/components/parts/TwitterButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-btn color="light-blue" class="text-capitalize" prepend-icon="mdi-twitter" @click.stop="props.onClick"
+    >Twitter</v-btn
+  >
+</template>
+
+<script setup lang="ts">
+  type Props = { onClick: () => void };
+
+  const props = defineProps<Props>();
+</script>
