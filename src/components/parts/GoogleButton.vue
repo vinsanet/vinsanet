df46e3b92a511f72d2fa36a81aeafbca9bbd3fff
hnymA <hnyma637111@gmail.com>
2022-12-19 18:10:13 +0900

    add google button

---
+++ src/components/parts/GoogleButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-btn color="red" class="text-capitalize" prepend-icon="mdi-google" @click.stop="props.onClick">Google</v-btn>
+</template>
+
+<script setup lang="ts">
+  type Props = { onClick: () => void };
+
+  const props = defineProps<Props>();
+</script>
