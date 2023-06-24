065d07506a65b827d545c837d42d0d9116a184b9
hnymA <hnyma637111@gmail.com>
2023-06-24 10:01:21 +0900

    add routing for document pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,23 @@
+import Document from "@/components/layouts/Document.vue";
 import Main from "@/components/layouts/Main.vue";
 import Simple from "@/components/layouts/Simple.vue";
 import Account from "@/components/pages/Account.vue";
 import CharactersIdEdit from "@/components/pages/CharactersIdEdit.vue";
 import CharactersIdView from "@/components/pages/CharactersIdView.vue";
+import Faq from "@/components/pages/Faq.vue";
 import Index from "@/components/pages/Index.vue";
+import Inquiry from "@/components/pages/Inquiry.vue";
 import Login from "@/components/pages/Login.vue";
 import Mypage from "@/components/pages/Mypage.vue";
 import Register from "@/components/pages/Register.vue";
+import Terms from "@/components/pages/Terms.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   { path: "/", component: Simple, children: [{ path: "", component: Index }] },
+  { path: "/faq", component: Document, children: [{ path: "", component: Faq }] },
+  { path: "/inquiry", component: Document, children: [{ path: "", component: Inquiry }] },
+  { path: "/terms", component: Document, children: [{ path: "", component: Terms }] },
   { path: "/login", component: Simple, children: [{ path: "", component: Login }] },
   { path: "/register", component: Simple, children: [{ path: "", component: Register }] },
   { path: "/account", component: Main, children: [{ path: "", component: Account }] },
