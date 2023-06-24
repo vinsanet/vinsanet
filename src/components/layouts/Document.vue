e74b89b5b865fbd5a151f0c20a1ec825b4d39974
hnymA <hnyma637111@gmail.com>
2023-06-24 10:00:51 +0900

    add document pages

---
+++ src/components/layouts/Document.vue
@@ -0,0 +1,22 @@
+<template>
+  <DocumentHeader :on-click-nav-icon="onClickNavIcon" />
+  <DocumentDrawer :drawer="drawer" />
+  <v-main>
+    <router-view />
+    <Snackbar />
+  </v-main>
+</template>
+
+<script setup lang="ts">
+  import DocumentDrawer from "@/components/parts/DocumentDrawer.vue";
+  import DocumentHeader from "@/components/parts/DocumentHeader.vue";
+  import Snackbar from "@/components/parts/Snackbar.vue";
+  import { ref } from "vue";
+  import { RouterView } from "vue-router";
+
+  const drawer = ref(false);
+
+  const onClickNavIcon = () => {
+    drawer.value = !drawer.value;
+  };
+</script>
---
+++ src/components/pages/Faq.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-container class="d-flex fill-height">
+    <v-row>
+      <v-col>
+        <v-row class="pa-2">
+          <v-col class="text-center">
+            <div :class="['text-h3']">よくある質問</div>
+          </v-col>
+        </v-row>
+        <v-row class="pa-2">
+          <v-divider></v-divider>
+        </v-row>
+        <v-row class="pa-2">
+          <v-col>
+            <v-card>
+              <v-card-title>質問1</v-card-title>
+              <v-card-text>
+                <v-row>
+                  <v-col>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                      esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+                      in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row class="pa-2">
+          <v-col>
+            <v-card>
+              <v-card-title>質問2</v-card-title>
+              <v-card-text>
+                <v-row>
+                  <v-col>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                      esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+                      in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row class="pa-2">
+          <v-col>
+            <v-card>
+              <v-card-title>質問3</v-card-title>
+              <v-card-text>
+                <v-row>
+                  <v-col>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                      esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+                      in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+  //
+</script>
