52a6f6fa9f749cf3298cb690e6721897feee19a8
hnymA <hnyma637111@gmail.com>
2023-06-25 17:27:03 +0900

    remove email authentication

--- src/components/pages/Login.vue
+++ src/components/pages/Login.vue
@@ -7,45 +7,18 @@
             <div :class="['text-h3']">ログイン</div>
           </v-col>
         </v-row>
-        <v-row :justify="'center'" class="pa-2">
-          <v-col cols="4">
-            <v-form>
-              <v-text-field
-                v-model="email"
-                label="Email"
-                type="text"
-                prepend-icon="mdi-email"
-                autocomplete="current-password"
-              />
-              <v-text-field
-                v-model="password"
-                label="Password"
-                type="password"
-                prepend-icon="mdi-key"
-                autocomplete="current-password"
-              />
-            </v-form>
-          </v-col>
-        </v-row>
-        <v-row :justify="'center'" class="pa-2">
-          <v-col class="text-center" cols="1">
-            <v-btn color="primary" @click="onClickLogin">ログイン</v-btn>
-          </v-col>
-        </v-row>
-        <v-row :justify="'center'" class="pa-2">
-          <v-divider></v-divider>
-        </v-row>
-        <v-row :justify="'center'" class="pa-2">
-          <v-col class="text-center">
-            <div :class="['text-h5']">SNSでログインする</div>
-          </v-col>
-        </v-row>
-        <v-row :justify="'center'" class="pa-2">
-          <v-col class="text-center" cols="1">
-            <TwitterButton :on-click="onClickTwitter" />
-          </v-col>
-          <v-col class="text-center" cols="1">
-            <GoogleButton :on-click="onClickGoogle" />
+        <v-row>
+          <v-col>
+            <v-row :justify="'center'" class="pa-2">
+              <v-col class="text-center" cols="1">
+                <TwitterButton :on-click="onClickTwitter" />
+              </v-col>
+            </v-row>
+            <v-row :justify="'center'" class="pa-2">
+              <v-col class="text-center" cols="1">
+                <GoogleButton :on-click="onClickGoogle" />
+              </v-col>
+            </v-row>
           </v-col>
         </v-row>
         <v-row :justify="'center'" class="pa-2">
@@ -63,21 +36,12 @@
   import GoogleButton from "@/components/parts/GoogleButton.vue";
   import TwitterButton from "@/components/parts/TwitterButton.vue";
   import { firebaseAuth } from "@/firebase/firebase";
-  import { firebaseErrorMessage } from "@/firebase/firebaseErrorMessage";
   import { useSnackbarStore } from "@/store/snackbar";
-  import {
-    GoogleAuthProvider,
-    TwitterAuthProvider,
-    signInWithEmailAndPassword,
-    signInWithRedirect,
-  } from "firebase/auth";
-  import { ref } from "vue";
+  import { GoogleAuthProvider, TwitterAuthProvider, signInWithRedirect } from "firebase/auth";
   import { useRouter } from "vue-router";
 
   const { showSnackbar } = useSnackbarStore();
 
-  const email = ref("");
-  const password = ref("");
   const router = useRouter();
 
   firebaseAuth.onAuthStateChanged((user) => {
@@ -85,21 +49,6 @@
     showSnackbar("ログインしました", "success");
     router.push("/mypage");
   });
-  const onClickLogin = () => {
-    if (email.value === "" || password.value == "") {
-      showSnackbar("メールアドレスとパスワードを入力してください", "info");
-      return;
-    }
-    signInWithEmailAndPassword(firebaseAuth, email.value, password.value)
-      .then(() => {
-        showSnackbar("ログインしました", "success");
-        router.push("/mypage");
-      })
-      .catch((error) => {
-        const errorMessage = firebaseErrorMessage(error.code);
-        showSnackbar(errorMessage, "error");
-      });
-  };
   const onClickTwitter = () => {
     const provider = new TwitterAuthProvider();
     signInWithRedirect(firebaseAuth, provider).catch((error) => {
--- src/components/pages/Register.vue
+++ src/components/pages/Register.vue
@@ -7,45 +7,18 @@
             <div :class="['text-h3']">新規登録</div>
           </v-col>
         </v-row>
-        <v-row :justify="'center'" class="pa-2">
-          <v-col cols="4">
-            <v-form>
-              <v-text-field
-                v-model="email"
-                label="Email"
-                type="text"
-                prepend-icon="mdi-email"
-                autocomplete="current-password"
-              />
-              <v-text-field
-                v-model="password"
-                label="Password"
-                type="password"
-                prepend-icon="mdi-key"
-                autocomplete="current-password"
-              />
-            </v-form>
-          </v-col>
-        </v-row>
-        <v-row :justify="'center'" class="pa-2">
-          <v-col class="text-center" cols="1">
-            <v-btn color="primary" @click.stop="onClickCreate">新規登録</v-btn>
-          </v-col>
-        </v-row>
-        <v-row :justify="'center'" class="pa-2">
-          <v-divider></v-divider>
-        </v-row>
-        <v-row :justify="'center'" class="pa-2">
-          <v-col class="text-center">
-            <div :class="['text-h5']">SNSで登録する</div>
-          </v-col>
-        </v-row>
-        <v-row :justify="'center'" class="pa-2">
-          <v-col class="text-center" cols="1">
-            <TwitterButton :on-click="onClickTwitter" />
-          </v-col>
-          <v-col class="text-center" cols="1">
-            <GoogleButton :on-click="onClickGoogle" />
+        <v-row>
+          <v-col>
+            <v-row :justify="'center'" class="pa-2">
+              <v-col class="text-center" cols="1">
+                <TwitterButton :on-click="onClickTwitter" />
+              </v-col>
+            </v-row>
+            <v-row :justify="'center'" class="pa-2">
+              <v-col class="text-center" cols="1">
+                <GoogleButton :on-click="onClickGoogle" />
+              </v-col>
+            </v-row>
           </v-col>
         </v-row>
         <v-row :justify="'center'" class="pa-2">
@@ -63,21 +36,12 @@
   import GoogleButton from "@/components/parts/GoogleButton.vue";
   import TwitterButton from "@/components/parts/TwitterButton.vue";
   import { firebaseAuth } from "@/firebase/firebase";
-  import { firebaseErrorMessage } from "@/firebase/firebaseErrorMessage";
   import { useSnackbarStore } from "@/store/snackbar";
-  import {
-    GoogleAuthProvider,
-    TwitterAuthProvider,
-    createUserWithEmailAndPassword,
-    signInWithRedirect,
-  } from "@firebase/auth";
-  import { ref } from "vue";
+  import { GoogleAuthProvider, TwitterAuthProvider, signInWithRedirect } from "@firebase/auth";
   import { useRouter } from "vue-router";
 
   const { showSnackbar } = useSnackbarStore();
 
-  const email = ref("");
-  const password = ref("");
   const router = useRouter();
 
   firebaseAuth.onAuthStateChanged((user) => {
@@ -85,21 +49,6 @@
     showSnackbar("アカウントを作成しました", "success");
     router.push("/mypage");
   });
-  const onClickCreate = () => {
-    if (email.value === "" || password.value == "") {
-      showSnackbar("メールアドレスとパスワードを入力してください", "info");
-      return;
-    }
-    createUserWithEmailAndPassword(firebaseAuth, email.value, password.value)
-      .then(() => {
-        showSnackbar("アカウントを作成しました", "success");
-        router.push("/mypage");
-      })
-      .catch((error) => {
-        const errorMessage = firebaseErrorMessage(error.code);
-        showSnackbar(errorMessage, "error");
-      });
-  };
   const onClickTwitter = () => {
     const provider = new TwitterAuthProvider();
     signInWithRedirect(firebaseAuth, provider).catch((error) => {
