82687bce226733e7b9393fe76880d61d58254d23
hnymA <hnyma637111@gmail.com>
2023-06-26 17:37:26 +0900

    add github authentication

--- src/components/pages/Account.vue
+++ src/components/pages/Account.vue
@@ -4,10 +4,10 @@
       <v-row>
         <v-col>
           <v-card>
-            <v-card-title>SNS認証</v-card-title>
+            <v-card-title>ログイン連携</v-card-title>
             <v-card-text>
               <v-row>
-                <v-col>現在のアカウントにSNSアカウントを連携することができます。</v-col>
+                <v-col>現在のアカウントに他サービスのアカウントを連携することができます。</v-col>
               </v-row>
               <v-row>
                 <v-table>
@@ -31,6 +31,17 @@
                         <v-btn :color="!googleAuthorized ? 'primary' : 'error'" @click.stop="onClickGoogle">
                           <v-icon>{{ !googleAuthorized ? "mdi-link" : "mdi-link-off" }}</v-icon>
                           {{ !googleAuthorized ? "連携する" : "連携解除" }}
+                        </v-btn>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <div :class="'text-subtitle-1'"><v-icon>mdi-github</v-icon>GitHub</div>
+                      </td>
+                      <td>
+                        <v-btn :color="!githubAuthoized ? 'primary' : 'error'" @click.stop="onClickGithub">
+                          <v-icon>{{ !githubAuthoized ? "mdi-link" : "mdi-link-off" }}</v-icon>
+                          {{ !githubAuthoized ? "連携する" : "連携解除" }}
                         </v-btn>
                       </td>
                     </tr>
@@ -77,12 +88,29 @@
       </v-card-actions>
     </v-card>
   </v-dialog>
+  <v-dialog v-model="githubDialog" width="30%" min-width="400px">
+    <v-card>
+      <v-card-title>GitHub連携解除</v-card-title>
+      <v-card-text>
+        <v-row>
+          <v-col>連携を解除すると、GitHubアカウントでログインできなくなります。よろしいですか？</v-col>
+        </v-row>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" @click="githubDialog = false"> キャンセル </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="error" @click="onClickUnLinkGithub">OK</v-btn>
+        <v-spacer></v-spacer>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script setup lang="ts">
   import { firebaseAuth } from "@/firebase/firebase";
   import { useSnackbarStore } from "@/store/snackbar";
-  import { GoogleAuthProvider, TwitterAuthProvider, linkWithRedirect, unlink } from "firebase/auth";
+  import { GithubAuthProvider, GoogleAuthProvider, TwitterAuthProvider, linkWithRedirect, unlink } from "firebase/auth";
   import { ref } from "vue";
   import { useRouter } from "vue-router";
 
@@ -91,8 +119,10 @@
 
   const twitterAuthorized = ref(false);
   const googleAuthorized = ref(false);
+  const githubAuthoized = ref(false);
   const twitterDialog = ref(false);
   const googleDialog = ref(false);
+  const githubDialog = ref(false);
 
   firebaseAuth.onAuthStateChanged((user) => {
     if (!user) {
@@ -105,6 +135,8 @@
         twitterAuthorized.value = true;
       } else if (provider.providerId === "google.com") {
         googleAuthorized.value = true;
+      } else if (provider.providerId === "github.com") {
+        githubAuthoized.value = true;
       }
     });
   });
@@ -115,18 +147,18 @@
       router.push("/login");
       return;
     }
-    if (!twitterAuthorized.value) {
-      linkWithRedirect(firebaseAuth.currentUser, provider)
-        .then(() => {
-          showSnackbar("Twitterアカウントを連携しました", "success");
-        })
-        .catch((error) => {
-          const errorMessage = error.message;
-          showSnackbar(`Twitterアカウントを連携できませんでした：${errorMessage}`, "error");
-        });
-    } else {
+    if (twitterAuthorized.value) {
       twitterDialog.value = true;
+      return;
     }
+    linkWithRedirect(firebaseAuth.currentUser, provider)
+      .then(() => {
+        showSnackbar("Twitterアカウントを連携しました", "success");
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        showSnackbar(`Twitterアカウントを連携できませんでした：${errorMessage}`, "error");
+      });
   };
   const onClickGoogle = () => {
     const provider = new GoogleAuthProvider();
@@ -135,18 +167,38 @@
       router.push("/login");
       return;
     }
-    if (!googleAuthorized.value) {
-      linkWithRedirect(firebaseAuth.currentUser, provider)
-        .then(() => {
-          showSnackbar("Googleアカウントを連携しました", "success");
-        })
-        .catch((error) => {
-          const errorMessage = error.message;
-          showSnackbar(`Googleアカウントを連携できませんでした：${errorMessage}`, "error");
-        });
-    } else {
+    if (googleAuthorized.value) {
       googleDialog.value = true;
+      return;
     }
+    linkWithRedirect(firebaseAuth.currentUser, provider)
+      .then(() => {
+        showSnackbar("Googleアカウントを連携しました", "success");
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        showSnackbar(`Googleアカウントを連携できませんでした：${errorMessage}`, "error");
+      });
+  };
+  const onClickGithub = () => {
+    const provider = new GithubAuthProvider();
+    if (!firebaseAuth.currentUser) {
+      showSnackbar("ログインしてください", "error");
+      router.push("/login");
+      return;
+    }
+    if (githubAuthoized.value) {
+      githubDialog.value = true;
+      return;
+    }
+    linkWithRedirect(firebaseAuth.currentUser, provider)
+      .then(() => {
+        showSnackbar("GitHubアカウントを連携しました", "success");
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        showSnackbar(`GitHubアカウントを連携できませんでした：${errorMessage}`, "error");
+      });
   };
   const onClickUnLinkTwitter = () => {
     const provider = new TwitterAuthProvider();
@@ -175,5 +227,19 @@
         showSnackbar(`Googleアカウントの連携を解除できませんでした：${errorMessage}`, "error");
       });
     googleDialog.value = false;
+  };
+  const onClickUnLinkGithub = () => {
+    const provider = new GithubAuthProvider();
+    if (!firebaseAuth.currentUser) return;
+    unlink(firebaseAuth.currentUser, provider.providerId)
+      .then(() => {
+        showSnackbar("GitHubアカウントの連携を解除しました", "success");
+        githubAuthoized.value = false;
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        showSnackbar(`GitHubアカウントの連携を解除できませんでした：${errorMessage}`, "error");
+      });
+    githubDialog.value = false;
   };
 </script>
