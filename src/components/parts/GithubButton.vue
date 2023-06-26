309c40f35cff237da98787233fa43f28b28ec7a7
hnymA <hnyma637111@gmail.com>
2023-06-26 17:37:54 +0900

    add GithubButton

---
+++ src/components/parts/GithubButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-btn color="grey-darken-4" class="text-capitalize" prepend-icon="mdi-github" @click.stop="props.onClick"
+    >GitHub</v-btn
+  >
+</template>
+
+<script setup lang="ts">
+  type Props = { onClick: () => void };
+
+  const props = defineProps<Props>();
+</script>
