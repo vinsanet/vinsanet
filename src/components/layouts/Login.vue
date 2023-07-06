3cf6b2e2395fe8c605c6af79b53074b2b4f33033
hnymA <hnyma637111@gmail.com>
2023-07-06 18:33:00 +0900

    add login layout

---
+++ src/components/layouts/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <LoginHeader />
+  <v-main>
+    <router-view />
+    <Snackbar />
+  </v-main>
+</template>
+
+<script setup lang="ts">
+  import LoginHeader from "@/components/parts/LoginHeader.vue";
+  import Snackbar from "@/components/parts/Snackbar.vue";
+  import { RouterView } from "vue-router";
+</script>
