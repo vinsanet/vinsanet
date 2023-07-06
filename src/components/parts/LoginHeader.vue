23d86318f464871ea8ba615fe92c0248ae1d2440
hnymA <hnyma637111@gmail.com>
2023-07-06 18:32:42 +0900

    add login header component

---
+++ src/components/parts/LoginHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app-bar hide-on-scroll>
+    <v-app-bar-title class="text-subtitle-1">
+      <div style="cursor: pointer" @click="$router.push('/')">
+        Kutulu character sheet <v-chip size="small">Beta</v-chip>
+        <div :class="['text-caption']">v{{ version }}</div>
+      </div>
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script setup lang="ts">
+  const version = import.meta.env.VITE_APP_VERSION;
+</script>
