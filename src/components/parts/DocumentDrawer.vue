55a709ade7991d065912bda65197a0563808a16f
hnymA <hnyma637111@gmail.com>
2023-07-13 22:05:27 +0900

    change rail mode in mobile

--- src/components/parts/DocumentDrawer.vue
+++ src/components/parts/DocumentDrawer.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-navigation-drawer :model-value="props.drawer" expand-on-hover rail>
+  <v-navigation-drawer :model-value="props.drawer" expand-on-hover :rail="!mobile">
     <v-list nav>
       <v-list-item prepend-icon="mdi-home" title="ホーム" value="home" @click="onClickHome"></v-list-item>
       <v-list-item prepend-icon="mdi-file-document" title="利用規約" value="terms" @click="onClickTerms"></v-list-item>
@@ -11,12 +11,13 @@
 
 <script setup lang="ts">
   import { useRouter } from "vue-router";
+  import { useDisplay } from "vuetify";
 
   type Props = { drawer: boolean };
 
   const props = defineProps<Props>();
   const router = useRouter();
-
+  const { mobile } = useDisplay();
   const onClickHome = () => {
     router.push("/");
     return;
