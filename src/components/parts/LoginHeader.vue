8df4ae9b24a60bfbfd8a1e98b7e7ee8d811ceb6e
hnymA <hnyma637111@gmail.com>
2023-07-13 22:02:09 +0900

    fix elevation value

--- src/components/parts/LoginHeader.vue
+++ src/components/parts/LoginHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app-bar hide-on-scroll>
+  <v-app-bar elevation="1">
     <v-app-bar-title class="text-subtitle-1">
       <div style="cursor: pointer" @click="$router.push('/')">
         Kutulu character sheet <v-chip size="small">Beta</v-chip>
