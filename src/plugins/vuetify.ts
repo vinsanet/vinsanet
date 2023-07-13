0b2312e6b0c9e21a29758da46c46de660ae89004
hnymA <hnyma637111@gmail.com>
2023-07-13 20:48:48 +0900

    define colors

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -7,8 +7,26 @@ export default createVuetify({
     themes: {
       light: {
         colors: {
-          primary: "#1867C0",
-          secondary: "#5CBBF6",
+          background: "#fffffc", // 胡粉色
+          primary: "#674196", // 菖蒲色
+          secondary: "#3e62ad", // 杜若色
+          success: "#007b43", // 常磐色
+          error: "#c9171e", // 深緋
+          skill: "#38b48b", // 翡翠色
+          speciality: "#007bbb", // 紺碧
+          injury: "#d9333f", // 紅赤
+        },
+      },
+      dark: {
+        colors: {
+          background: "#16160e", // 暗黒色
+          primary: "#9f7acc",
+          secondary: "#687ead",
+          success: "#497a64",
+          error: "#c9797c",
+          skill: "#6db59d",
+          speciality: "#70a0ba",
+          injury: "#d98288",
         },
       },
     },
