846676f58e313567cab29c835d3fbb74fa6e5bd4
hnymA <hnyma637111@gmail.com>
2023-07-17 10:22:22 +0900

    add not found page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import DocumentLayout from "@/components/layouts/Document.vue";
-import LoginLayout from "@/components/layouts/Login.vue";
+import IndexLayout from "@/components/layouts/Index.vue";
 import MainLayout from "@/components/layouts/Main.vue";
 import SimpleLayout from "@/components/layouts/Simple.vue";
 import Account from "@/components/pages/Account.vue";
@@ -9,16 +9,17 @@ import Faq from "@/components/pages/Faq.vue";
 import Index from "@/components/pages/Index.vue";
 import Login from "@/components/pages/Login.vue";
 import Mypage from "@/components/pages/Mypage.vue";
+import NotFound from "@/components/pages/NotFound.vue";
 import Register from "@/components/pages/Register.vue";
 import Terms from "@/components/pages/Terms.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-  { path: "/", component: SimpleLayout, children: [{ path: "", component: Index }] },
+  { path: "/", component: IndexLayout, children: [{ path: "", component: Index }] },
   { path: "/faq", component: DocumentLayout, children: [{ path: "", component: Faq }] },
   { path: "/terms", component: DocumentLayout, children: [{ path: "", component: Terms }] },
-  { path: "/login", component: LoginLayout, children: [{ path: "", component: Login }] },
-  { path: "/register", component: LoginLayout, children: [{ path: "", component: Register }] },
+  { path: "/login", component: SimpleLayout, children: [{ path: "", component: Login }] },
+  { path: "/register", component: SimpleLayout, children: [{ path: "", component: Register }] },
   { path: "/account", component: MainLayout, children: [{ path: "", component: Account }] },
   {
     path: "/characters/:id",
@@ -29,6 +30,7 @@ const routes = [
     ],
   },
   { path: "/mypage", component: MainLayout, children: [{ path: "", component: Mypage }] },
+  { path: "/:catchAll(.*)", component: SimpleLayout, children: [{ path: "", component: NotFound }] },
 ];
 
 const router = createRouter({
