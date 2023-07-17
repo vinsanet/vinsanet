697e522b837c4ced3a47b61b89785a0a5eaa0420
hnymA <hnyma637111@gmail.com>
2023-07-17 10:22:47 +0900

    update icons and card height

--- src/components/pages/Index.vue
+++ src/components/pages/Index.vue
@@ -27,24 +27,24 @@
       </v-row>
       <v-row class="align-center text-center align-stretch">
         <v-col>
-          <v-card>
-            <v-icon size="75">mdi-account-details</v-icon>
+          <v-card class="h-100">
+            <v-icon size="75">mdi-account-edit</v-icon>
             <v-card-title>キャラクター管理</v-card-title>
             <v-card-text>Kutulu TRPGのキャラクターを簡単に作成および編集することができます</v-card-text>
           </v-card>
         </v-col>
         <v-col>
-          <v-card>
-            <v-icon size="75">mdi-card-search-outline</v-icon>
+          <v-card class="h-100">
+            <v-icon size="75">mdi-folder-search</v-icon>
             <v-card-title>キャラクター閲覧</v-card-title>
             <v-card-text>作成したキャラクターを他の人に共有することも、非公開にすることもできます</v-card-text>
           </v-card>
         </v-col>
         <v-col>
-          <v-card>
+          <v-card class="h-100">
             <v-icon size="75">mdi-export</v-icon>
-            <v-card-title>CCFOLIAコマ出力</v-card-title>
-            <v-card-text>作成したキャラクターをCCFOLIAへ出力できます</v-card-text>
+            <v-card-title>キャラクター出力</v-card-title>
+            <v-card-text>作成したキャラクターをCCFOLIAへ出力したり、ファイルに保存することができます</v-card-text>
           </v-card>
         </v-col>
       </v-row>
