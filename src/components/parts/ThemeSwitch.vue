132250bfcdfc91f3eedca9c91619f9f5f8519f85
hnymA <hnyma637111@gmail.com>
2023-07-28 21:12:25 +0900

    add ThemeSwitch component

---
+++ src/components/parts/ThemeSwitch.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row justify="center" class="align-center">
+    <v-col class="text-center"><v-icon>mdi-weather-sunny</v-icon></v-col>
+    <v-col class="text-center pl-0">
+      <v-switch hide-details :model-value="theme === 'dark'" inset @change="toggleTheme"></v-switch>
+    </v-col>
+    <v-col class="text-center"><v-icon>mdi-weather-night</v-icon></v-col>
+  </v-row>
+</template>
+
+<script setup lang="ts">
+  import { useThemeStore } from "@/store/theme";
+  import { storeToRefs } from "pinia";
+
+  const themeStore = useThemeStore();
+  const { theme } = storeToRefs(themeStore);
+  const { toggleTheme } = themeStore;
+</script>
