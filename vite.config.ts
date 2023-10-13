ce5e21a193f01333b4ad7a7fcdb20c2bd0d09977
hnymA <hnyma637111@gmail.com>
2023-10-13 19:20:07 +0900

    reformat

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,15 @@
-// Plugins
 import vue from "@vitejs/plugin-vue";
-// Utilities
 import { URL, fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import vuetify from "vite-plugin-vuetify";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify({
-      autoImport: true,
-    }),
-  ],
+  plugins: [vue(), vuetify({ autoImport: true })],
   define: { "process.env": {} },
   resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+    alias: { "@": fileURLToPath(new URL("./src", import.meta.url)) },
     extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
   },
-  server: {
-    port: 3000,
-  },
+  build: { manifest: true },
+  server: { port: 3000 },
 });
