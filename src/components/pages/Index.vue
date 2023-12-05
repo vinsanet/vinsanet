313cb56fea8fb981355cfbdbdb90c993c8722ad7
hnymA <hnyma637111@gmail.com>
2023-12-05 21:24:32 +0900

    update with new logo and background

--- src/components/pages/Index.vue
+++ src/components/pages/Index.vue
@@ -1,81 +1,81 @@
 <template>
   <v-theme-provider theme="light" with-background>
-    <v-parallax :src="imageUrl">
-      <v-container class="fill-height">
-        <v-row style="height: 100vh" class="align-center text-center">
-          <v-col>
-            <v-row>
-              <v-col>
-                <div :class="['text-h2', 'text-lg-h1']">vinsanet <v-chip>Beta</v-chip></div>
-              </v-col>
-            </v-row>
-            <v-row class="mb-2">
-              <v-col><div class="text-subtitle-1 font-weight-bold">”信頼できるキャラクターシート”</div></v-col>
-            </v-row>
-            <v-row :justify="'center'" class="ma-4">
-              <v-spacer></v-spacer>
-              <v-col><v-btn color="primary" variant="flat" to="/login" block>ログイン</v-btn></v-col>
-              <v-col><v-btn color="secondary" variant="flat" to="/register" block>新規登録</v-btn></v-col>
-              <v-spacer></v-spacer>
-            </v-row>
-            <v-row class="my-4">
-              <v-divider></v-divider>
-            </v-row>
-            <v-row>
-              <v-col>
-                <p>vinsanetは、Kutulu TRPGのキャラクターシートを簡単に作成、管理、共有できるオンラインツールです。</p>
-                <p>ご利用にはアカウント登録が必要です。料金は掛かりません。</p>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col></v-col>
-            </v-row>
-          </v-col>
-        </v-row>
-        <v-row class="align-center text-center align-stretch">
-          <v-col>
-            <v-card class="h-100" variant="flat">
-              <v-icon size="75">mdi-account-edit</v-icon>
-              <v-card-title>キャラクター管理</v-card-title>
-              <v-card-text>Kutulu TRPGのキャラクターを簡単に作成および編集することができます</v-card-text>
-            </v-card>
-          </v-col>
-          <v-col>
-            <v-card class="h-100" variant="flat">
-              <v-icon size="75">mdi-folder-search</v-icon>
-              <v-card-title>キャラクター閲覧</v-card-title>
-              <v-card-text>作成したキャラクターを他の人に共有することも、非公開にすることもできます</v-card-text>
-            </v-card>
-          </v-col>
-          <v-col>
-            <v-card class="h-100" variant="flat">
-              <v-icon size="75">mdi-export</v-icon>
-              <v-card-title>キャラクター出力</v-card-title>
-              <v-card-text>作成したキャラクターをCCFOLIAへ出力したり、ファイルに保存することができます</v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
-        <v-row class="align-center text-center">
-          <v-col>
-            <v-card color="blue-grey" variant="flat">
-              <v-card-text class="text-left pa-6">
-                <p>
-                  本サイトは、株式会社viviONより発売された、Kutulu日本語版の２次創作ガイドラインに基づく創作物です。オリジナル（スウェーデン語）の利用はCC-BY4.0によります。
-                </p>
-                <p>
-                  "Kutulu" is available under the CC-BY 4.0 International license; therefore, anyone is free to create,
-                  copy and add to it as long as they follow the legal norms of that license.
-                </p>
-                <p>
-                  "Kutulu Japanese Language Version" is Copyright 2022 By ViviON ; All Rights Reserved. Planning By
-                  FrogGames. Published By ViviON.
-                </p>
-              </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-parallax>
+    <v-container class="fill-height">
+      <v-row style="height: 100vh" class="align-center text-center">
+        <v-col>
+          <v-row>
+            <v-col>
+              <div class="d-flex justify-center">
+                <v-img :src="logoImageUrl" max-width="700px"></v-img>
+              </div>
+            </v-col>
+          </v-row>
+          <v-row class="mb-2">
+            <v-col><div class="text-subtitle-1 font-weight-bold">”信頼できるキャラクターシート”</div></v-col>
+          </v-row>
+          <v-row :justify="'center'" class="ma-4">
+            <v-spacer></v-spacer>
+            <v-col><v-btn color="primary" variant="flat" to="/login" block>ログイン</v-btn></v-col>
+            <v-col><v-btn color="secondary" variant="flat" to="/register" block>新規登録</v-btn></v-col>
+            <v-spacer></v-spacer>
+          </v-row>
+          <v-row class="my-4">
+            <v-divider></v-divider>
+          </v-row>
+          <v-row>
+            <v-col>
+              <p>vinsanetは、Kutulu TRPGのキャラクターシートを簡単に作成、管理、共有できるオンラインツールです。</p>
+              <p>ご利用にはアカウント登録が必要です。料金は掛かりません。</p>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col></v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-row class="align-center text-center align-stretch">
+        <v-col>
+          <v-card class="h-100" variant="outlined">
+            <v-icon size="75">mdi-account-edit</v-icon>
+            <v-card-title>キャラクター管理</v-card-title>
+            <v-card-text>Kutulu TRPGのキャラクターを簡単に作成および編集することができます</v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card class="h-100" variant="outlined">
+            <v-icon size="75">mdi-folder-search</v-icon>
+            <v-card-title>キャラクター閲覧</v-card-title>
+            <v-card-text>作成したキャラクターを他の人に共有することも、非公開にすることもできます</v-card-text>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card class="h-100" variant="outlined">
+            <v-icon size="75">mdi-export</v-icon>
+            <v-card-title>キャラクター出力</v-card-title>
+            <v-card-text>作成したキャラクターをCCFOLIAへ出力したり、ファイルに保存することができます</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row class="align-center text-center">
+        <v-col>
+          <v-card color="blue-grey" variant="flat">
+            <v-card-text class="text-left pa-6">
+              <p>
+                本サイトは、株式会社viviONより発売された、Kutulu日本語版の２次創作ガイドラインに基づく創作物です。オリジナル（スウェーデン語）の利用はCC-BY4.0によります。
+              </p>
+              <p>
+                "Kutulu" is available under the CC-BY 4.0 International license; therefore, anyone is free to create,
+                copy and add to it as long as they follow the legal norms of that license.
+              </p>
+              <p>
+                "Kutulu Japanese Language Version" is Copyright 2022 By ViviON ; All Rights Reserved. Planning By
+                FrogGames. Published By ViviON.
+              </p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
     <v-footer color="primary">
       <v-row no-gutters>
         <v-col v-for="link in links" :key="link.name" class="text-center">
@@ -96,7 +96,7 @@
 </template>
 
 <script setup lang="ts">
-  import imageUrl from "@/assets/index.jpg";
+  import logoImageUrl from "@/assets/logo_white.png";
   import { onMounted, ref } from "vue";
 
   const links = ref([
